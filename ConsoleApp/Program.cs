c1a6b485999f281e968a17a1a4ad6fe5f91577dd
kaw393939 <keith@webizly.com>
2019-12-09 09:21:30 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleHelper;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtilities consoleUtilities = new ConsoleUtilities();
+
+            consoleUtilities.WriteLine("Hello World");
+
+        }
+    }
+}
